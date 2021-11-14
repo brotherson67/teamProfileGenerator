dae8b67d7ad56a300f6f5b7796ada91bd2b16a76
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-14 16:11:59 -0700

    changed orginizartion

--- index.js
+++
@@ -1,9 +0,0 @@
-class employee {
-    constructor(name, position, employeeID, email, github){
-        this.name = name; 
-        this.position = position;
-        this.employeeID = employeeID;
-        this.email = email;
-        this.github = github;
-    }
-}
