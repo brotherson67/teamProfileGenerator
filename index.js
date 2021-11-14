f2027ad61167f7d4267891e9b614c1ae25f5951f
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-14 12:10:12 -0700

    created employee class

---
+++ index.js
@@ -0,0 +1,9 @@
+class employee {
+    constructor(name, position, employeeID, email, github){
+        this.name = name; 
+        this.position = position;
+        this.employeeID = employeeID;
+        this.email = email;
+        this.github = github;
+    }
+}
