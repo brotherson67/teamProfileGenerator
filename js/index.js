0acd4e88280baeade08e0344ee08e2dddc9b90ec
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-21 20:55:47 -0700

    started over in single file

--- js/index.js
+++ js/index.js
@@ -1,42 +1,96 @@
+// global variables/imports
 const fs = require('fs');
 const inquirer = require ('inquirer');
-const employeeObjects = require('./employeeTypes');
 
 
+// create questions array for user
+let questions = [
+    {
+        type: 'input',
+        name: 'manager-name', 
+        message: 'What is the managers name?'
+    },
+    {
+        type: 'input',
+        name: 'employee-name', 
+        message: 'What is the employees name name?'
+    },
+    {
+        type: 'input',
+        name: 'employeeID', 
+        message: 'What is their employee ID?'
+    },{
+        type: 'input',
+        name: 'email', 
+        message: 'What is their email address?'
+    },{
+        type: 'input',
+        name: 'github', 
+        message: 'What is their GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber', 
+        message: 'What is their office number?'
+    },
+    {
+        type: 'list',
+        message: 'What is the managers name?',
+        name: 'employeePosition', 
+        choices: ['Engineer', 'Intern', "I don't have any more employees to add"]
+    },
+]
 
-// TODO: Create a function to write README file
-function writeToFile(answers) {
-    fs.writeFile(`teamProfileGen.HTML`, answers, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
-    } 
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-        .prompt(employeeObjects.questions)
-        .then(function(answers) {
-            const keepGoing = answers.employeeTitle;
-
-            // generate a card that is just for the manager
-            // then continue and see if next item is needed 
-            // if the answer is not index it 2 ('no more employees') add more cards to the page 
-            if (!keepGoing[2]){
-                let addCard = answers.employee
-            }
-            const generateFile = generateMarkdown(answers)
-
-            writeToFile(fileName, generateFile, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log('nice job man, you finally got it')
-            })
-        })
+// classes 
+class employee {
+    constructor(name, position, employeeID, email, github, school){
+        this.name = name; 
+        this.position = position;
+        this.employeeID = employeeID;
+        this.email = email;
+        this.github = github;
+        this.school = school;
+    }
     
-}
+};
 
-// Function call to initialize app
-init();
+
+// Functions
+
+const startHTML = () => {
+    // place all html until after nav div
+};
+
+const newDiv = (employeeTitle) => {
+    const employeeName = employee.name;
+    const employeePosition = employee.position;
+    const employeeID = employee.employeeID;
+    const employeeEmail = employee.email;
+    const employeeGithub = employee.github;
+    const employeeSchool = employee.school;
+    let divContents = "";
+    if (employeeTitle === "Engineer"){
+        divContents = 
+        `<div class="card" style="width: 18rem;">
+            <h2>${employeeName}</h2>>
+            <h3>${employeePosition}</h3>
+            <ul>
+                <li>${employeeID}</li>
+                <li>${employeeEmail}</li>
+                <li>${employeeGithub}</li>
+            </ul>
+        </div>`
+    }
+    if (employeeTitle === "Intern"){
+        divContents = 
+        `<div class="card" style="width: 18rem;">
+            <h2>${employeeName}</h2>>
+            <h3>${employeePosition}</h3>
+            <ul>
+                <li>${employeeID}</li>
+                <li>${employeeEmail}</li>
+                <li>${employeeSchool}</li>
+            </ul>
+        </div>`
+    }
+}
