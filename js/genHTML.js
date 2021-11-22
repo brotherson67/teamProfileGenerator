e7a9edcbeaba7b53a21b18c33d225c0f89364ff3
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-21 19:40:46 -0700

    little confused on where to go

--- js/genHTML.js
+++ js/genHTML.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const inquirer = require ('inquirer');
-const employeeObjects = require('./employeeTypes');
-
 //create function to start HTML template
 const startPage = () => {
     const html = 
@@ -40,40 +36,3 @@ const addDiv = employeeTitle => {
         </div>`
     }
 }
-
-// TODO: Create a function to write README file
-function writeToFile(answers) {
-    fs.writeFile(`teamProfileGen.HTML`, answers, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
-    } 
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer
-        .prompt(employeeObjects.questions)
-        .then(function(answers) {
-            const keepGoing = answers.employeeTitle;
-
-            // generate a card that is just for the manager
-            // then continue and see if next item is needed 
-            // if the answer is not index it 2 ('no more employees') add more cards to the page 
-            if (!keepGoing[2]){
-                let addCard = answers.employee
-            }
-            const generateFile = generateMarkdown(answers)
-
-            writeToFile(fileName, generateFile, (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log('nice job man, you finally got it')
-            })
-        })
-    
-}
-
-// Function call to initialize app
-init();
