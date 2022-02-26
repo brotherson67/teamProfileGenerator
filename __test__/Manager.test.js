d31e433c145de8333b9321fe1b20cd290cfe18c6
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-26 15:00:02 -0700

    changed manager test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,16 +1,10 @@
-const Employee = require("../js/employeeTypes");
-
-test("test if returned value is a string ", () => {
-  const employee = new Employee();
-
-  expect(employee.job).toBe(expect.any(String));
-});
+const Manager = require("../js/employeeTypes");
 
 test("are the values for the Name, address, email and id string ", () => {
   const manager = new Manager();
 
   expect(manager.name).toBe(expect.any(String));
   expect(manager.employeeId).toBe(expect.any(String));
-  expect(manager.address).toBe(expect.any(String));
+  expect(manager.emailAddress).toBe(expect.any(String));
   expect(manager.officeNumber).toBe(expect.any(String));
 });
