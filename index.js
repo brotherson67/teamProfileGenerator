b9fcf2f754125b10c972553e6fe6817e0944e349
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-26 14:54:47 -0700

    changed manager class)

--- index.js
+++ index.js
@@ -2,55 +2,20 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// Initialize inquireer 2x
-// the first time will be to ask the manager how many employees they have
-// the second will be to generate the HTML page based on the response
-// create an if statement within a for loo
-// if i = 0- then create a manager page
-// if i > 0 and  < response, create engineer
-// if i > response create intern
+var inquirer = require("inquirer");
+inquirer
+  // can i inansiate an due the manager/other question methods here?
 
-//initialize inquirer
-// add method to ask questions
-// get managers name
-// get managers email
-// get managers id number
-// give option to add, engineer, intern, employee or be done
-
-//create employee question its possible to make this a class
-// get
-
-//        </nav>`;
-//   fs.writeFile("./src/tests/team.html", html, function (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// }
-
-// function endHTML() {
-//   let divContents = `</body>
-//     </html>`;
-// }
-
-// function addOrExit() {
-//   inquirer.prompt(continueQuestions).then(function (data) {
-//     if (data.addOrExit === "Yes") {
-//       addEmployee();
-//     } else {
-//       endHTML();
-//     }
-//   });
-// }
-// // I've heard of this thing called recursion and I tried to implement it here.
-// function addEmployee() {
-//   inquirer.prompt(questions).then(function (data) {
-//     newDiv();
-
-//     addOrExit();
-//   });
-// }
-
-// function init() {
-//   startHTML();
-// }
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
