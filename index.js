037ce6c78855782eda26d0d23b36635607223885
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 00:56:14 -0700

    made managerInfo return a new object

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // global variables/imports
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { Manager } = require("./lib");
+const { managerInfo, engineerInfo, interInfo } = require("./lib/index");
 
-const manager = new Manager();
-// console.log(manager.questions());
-manager.questions();
+managerInfo();
+// inquirer
+//   // can i inansiate an due the manager/other question methods here?
 
-inquirer
-  // can i inansiate an due the manager/other question methods here?
-
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
