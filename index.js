2cf895e0c99454b6700a24499ae1510df63c5b49
Mitch Work1"git commit -m"page template <mitchell.brotherson@loanpro.io>
2022-02-28 12:48:54 -0700

    deleted html templates and created template.js

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const { managerInfo, engineerInfo, interInfo } = require("./lib/index");
 
 console.log(managerInfo().role);
 
-// inquirer
-//   // can i inansiate an due the manager/other question methods here?
+inquirer
+// can i inansiate an due the manager/other question methods here?
 
-//   .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then((answers) => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
+    .prompt([
+        /* Pass your questions in here */
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
