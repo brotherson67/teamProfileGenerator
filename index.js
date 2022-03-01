3d35a2e7d5958b5d063691aeaf2eaf6de765c009
Mitch Work1"git commit -m"page template <mitchell.brotherson@loanpro.io>
2022-03-01 12:41:13 -0700

    added manager questions function to index

--- index.js
+++ index.js
@@ -5,19 +5,39 @@ const { managerInfo, engineerInfo, interInfo } = require("./lib/index");
 
 // console.log(managerInfo().role);
 
-inquirer
-// can i inansiate an due the manager/other question methods here?
+const managerQuestions = () => {
+    inquirer
+    // can i inansiate an due the manager/other question methods here?
 
-    .prompt([
-        /* Pass your questions in here */
-    ])
-    .then((answers) => {
-        // Use user feedback for... whatever!!
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
-    });
+        .prompt([{
+                type: 'input',
+                name: "managerName",
+                message: "Hi what's your name?"
+            },
+            {
+                type: 'input',
+                name: "officeNumber",
+                message: "what's your office number"
+            },
+            {
+                type: "intput",
+                name: "managerEmail",
+                message: "What's your email?"
+            },
+            {
+                type: "input",
+                name: "employeeId",
+                message: "what's your employee id?"
+            }
+        ])
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
